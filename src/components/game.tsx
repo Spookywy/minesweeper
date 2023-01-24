8fb8f483f14f965c1390140eb8a5e789dac13103
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2023-01-24 16:48:32 +0100

    :sparkles: add mines generation to the board

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -16,13 +16,36 @@ export default function Game({
     boardWidth,
     numberOfMines,
 }: GameProps) {
+    const generateMinesLocation = (): number[][] => {
+        const possibleLocations = [...Array(boardHeight * boardWidth)].map(
+            (x, index) => [
+                (index - (index % boardHeight)) / boardHeight,
+                index % boardHeight,
+            ]
+        );
+        const pickedLocations = Array();
+        for (let i = 0; i < numberOfMines; i++) {
+            const location = Math.floor(
+                Math.random() * possibleLocations.length
+            );
+            pickedLocations.push(possibleLocations[location]);
+            possibleLocations.splice(location, 1);
+        }
+        return pickedLocations;
+    };
+
     const initializeBoard = (): Cell[][] => {
-        return Array(boardHeight).fill(
-            Array(boardWidth).fill({
+        const minesLocations = generateMinesLocation();
+        const board = [...Array(boardHeight)].map(() =>
+            [...Array(boardWidth)].map(() => ({
                 status: CellStatus.Hidden,
                 isMined: false,
-            })
+            }))
         );
+        for (let i = 0; i < minesLocations.length; i++) {
+            board[minesLocations[i][0]][minesLocations[i][1]].isMined = true;
+        }
+        return board;
     };
 
     const [board, setBoard] = useState(initializeBoard());
